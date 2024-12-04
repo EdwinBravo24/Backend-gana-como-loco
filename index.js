ed03f4c4a12091e4258db5f9100ba183fd4c5670
Edwin Bravo <edwinbravo2121@gmail.com>
2024-12-04 03:43:40 -0500

    cambios en index y vercel.json

--- index.js
+++ index.js
@@ -14,26 +14,13 @@ const PORT = process.env.PORT || 4000;
 // Configuración general
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-app.use(cors({
-    origin: [
-      'https://frontend-gana-como-loco.vercel.app', 
-      'http://localhost:4000' // Si también pruebas localmente
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  }));
+app.use(cors());
+
 // Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    connectTimeoutMS: 30000, // 30 segundos
-    socketTimeoutMS: 45000  // 45 segundos
-  }).then(() => {
-    console.log('Conectado a MongoDB');
-  }).catch(err => {
-    console.error('Error de conexión a MongoDB:', err);
-  });
+mongoose.connect(process.env.MONGO_URI, {})
+    .then(() => console.log('Conectado a MongoDB Atlas'))
+    .catch(err => console.error('Error de conexión a MongoDB:', err));
+
 // Rutas
 app.use('/v1/users', userRoutes);
 app.use('/v1/archivos', archivosRoutes); // Rutas de archivos
